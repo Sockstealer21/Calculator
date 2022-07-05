fba0ea02b35bb1e6567497f45ce33985479d3396
Sockstealer21 <michaelsuushi@gmail.com>
2022-07-05 14:33:30 -0700

    Create event listeners for all functions for 2 variable calculator use

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -26,9 +26,14 @@ operate = function(symbol,a,b){
     }
 }
 
+clearing = function(){
+    display.value = ''
+}
+
 
 const display = document.querySelector('input') 
 const numberBtn = document.querySelectorAll('.number')
+// functionBtns below
 const addBtn = document.querySelector('#add')
 const subtractBtn = document.querySelector('#subtract')
 const multiplyBtn = document.querySelector('#multiply')
@@ -36,11 +41,50 @@ const divideBtn = document.querySelector('#divide')
 const equalsBtn = document.querySelector('#equals')
 const clearBtn = document.querySelector('#clear')
 
+let num1 = display.value
+let num2 
+let operateBtn
+
 numberBtn.forEach((button) =>{
     button.addEventListener('click', () =>{
         display.value += button.textContent
     })
 })
+
+addBtn.addEventListener('click', () =>{
+    num1 = parseInt(display.value)
+    clearing()
+    operateBtn = '+'
+})
+
+subtractBtn.addEventListener('click', () =>{
+    num1 = parseInt(display.value)
+    clearing()
+    operateBtn = '-'
+})
+
+multiplyBtn.addEventListener('click', () =>{
+    num1 = parseInt(display.value)
+    clearing()
+    operateBtn = '*'
+})
+
+divideBtn.addEventListener('click', () =>{
+    num1 = parseInt(display.value)
+    clearing()
+    operateBtn = '/'
+})
+
+clearBtn.addEventListener('click', clearing)    
+
+equalsBtn.addEventListener('click', ()=>{
+    num2 = parseInt(display.value)
+    display.value = operate(operateBtn,num1,num2)
+})
+
+
+
+
 
 
 
