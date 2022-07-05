77ce259d82665a524024ed1bf61e9970e2454ddf
Sockstealer21 <michaelsuushi@gmail.com>
2022-07-04 23:46:16 -0700

    Assign id to each function btn and number class to each numberBtn also click event for numberBtn

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -26,3 +26,15 @@ operate = function(symbol,a,b){
     }
 }
 
+
+const display = document.querySelector('input') 
+const numberBtn = document.querySelectorAll('.number')
+
+numberBtn.forEach((button) =>{
+    button.addEventListener('click', () =>{
+        display.value += button.textContent
+    })
+})
+    
+
+// Add event listener on all buttons to update display.value to their textcontent
