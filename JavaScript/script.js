376b1bd199207024fbbe38a2c4d01c3e5efaca21
Sockstealer21 <michaelsuushi@gmail.com>
2022-07-05 14:58:37 -0700

    Fixed all operator selectors in script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -30,19 +30,23 @@ clearing = function(){
     display.value = ''
 }
 
+numberinsert = function(){
+    numberArray.push(num)
+    clearing()
+
+}
 
 const display = document.querySelector('input') 
 const numberBtn = document.querySelectorAll('.number')
 // functionBtns below
-const addBtn = document.querySelector('#add')
-const subtractBtn = document.querySelector('#subtract')
-const multiplyBtn = document.querySelector('#multiply')
-const divideBtn = document.querySelector('#divide')
-const equalsBtn = document.querySelector('#equals')
-const clearBtn = document.querySelector('#clear')
+const addBtn = document.getElementById('+')
+const subtractBtn = document.getElementById('-')
+const multiplyBtn = document.getElementById('*')
+const divideBtn = document.getElementById('/')
+const equalsBtn = document.etElementById('=')
+const clearBtn = document.getElementById('Clear')
 
-let num1 = display.value
-let num2 
+let num = display.value
 let operateBtn
 let numberArray = []
 
@@ -53,25 +57,25 @@ numberBtn.forEach((button) =>{
 })
 
 addBtn.addEventListener('click', () =>{
-    num1 = parseInt(display.value)
+    num = parseInt(display.value)
     clearing()
     operateBtn = '+'
 })
 
 subtractBtn.addEventListener('click', () =>{
-    num1 = parseInt(display.value)
+    num = parseInt(display.value)
     clearing()
     operateBtn = '-'
 })
 
 multiplyBtn.addEventListener('click', () =>{
-    num1 = parseInt(display.value)
+    num = parseInt(display.value)
     clearing()
     operateBtn = '*'
 })
 
 divideBtn.addEventListener('click', () =>{
-    num1 = parseInt(display.value)
+    num = parseInt(display.value)
     clearing()
     operateBtn = '/'
 })
@@ -80,7 +84,7 @@ clearBtn.addEventListener('click', clearing)
 
 equalsBtn.addEventListener('click', ()=>{
     num2 = parseInt(display.value)
-    display.value = operate(operateBtn,num1,num2)
+    display.value = operate(operateBtn,num,num2)
 })
 
 
